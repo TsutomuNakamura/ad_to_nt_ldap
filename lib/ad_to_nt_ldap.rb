20349eea37c24df71e79f5724eb756fae2103391
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-11-25 21:33:06 +0900

    Updated

--- lib/ad_to_nt_ldap.rb
+++ lib/ad_to_nt_ldap.rb
@@ -3,4 +3,9 @@ require "ad_to_nt_ldap/version"
 module AdToNtLdap
   class Error < StandardError; end
   # Your code goes here...
+  class AdToNtLdap
+    def display
+      "Hello world"
+    end
+  end
 end
