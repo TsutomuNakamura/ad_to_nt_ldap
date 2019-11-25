1ea248dcc8d0507741afc1ca6bfc149af3566fcf
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-11-25 21:10:36 +0900

    First commit

---
+++ lib/ad_to_nt_ldap.rb
@@ -0,0 +1,6 @@
+require "ad_to_nt_ldap/version"
+
+module AdToNtLdap
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "ad_to_nt_ldap"
+
+require "minitest/autorun"
