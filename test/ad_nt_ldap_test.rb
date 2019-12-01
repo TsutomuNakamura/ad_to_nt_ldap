4e05db9b0efe4ce2885d09bb4610dbd9d4955047
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-01 19:05:25 +0900

    Updated

--- test/ad_nt_ldap_test.rb
+++ test/ad_nt_ldap_test.rb
@@ -5,16 +5,33 @@ class ModAdNtLdapTest < Minitest::Test
   #  refute_nil ::ModAdNtLdap::VERSION
   #end
 
-  def test_it_does_something_useful
-    adnt = AdNtLdap.new({:foo => "bar"})
-    puts adnt.display()
-  end
-
   def test_raise_error_if_params_is_nil
     exception = assert_raises RuntimeError do
       AdNtLdap.new(nil)
     end
-    puts exception.message
     assert_equal(exception.message, "Initialize AdNtLdap was failed. params must not be nil")
   end
+
+  def test_raise_error_if_params_doesnt_have_ad_host
+    exception = assert_raises RuntimeError do
+      AdNtLdap.new({
+        :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+        :ad_basedn => "CN=Users,DC=mysite,DC=example,DC=com",
+        :nt_host   => "192.168.1.12",
+        :nt_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
+        :nt_basedn => "dc=mysite,dc=example,dc=com"
+      })
+    end
+  end
+
+#  def test_it_does_something_useful
+#    adnt = AdNtLdap.new({
+#      :ad_host => "192.168.1.10",
+#      :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+#      :ad_basedn => "DC=mysite,DC=example,DC=com",
+#    })
+#    puts adnt.display()
+#  end
+
+
 end
