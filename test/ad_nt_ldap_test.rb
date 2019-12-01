7152b605c42b862250266eadc7614166f3bcf469
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-01 18:39:09 +0900

    Updated

--- test/ad_nt_ldap_test.rb
+++ test/ad_nt_ldap_test.rb
@@ -1,11 +1,20 @@
 require "test_helper"
 
 class ModAdNtLdapTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ModAdNtLdap::VERSION
-  end
+  #def test_that_it_has_a_version_number
+  #  refute_nil ::ModAdNtLdap::VERSION
+  #end
 
   def test_it_does_something_useful
-    assert false
+    adnt = AdNtLdap.new({:foo => "bar"})
+    puts adnt.display()
+  end
+
+  def test_raise_error_if_params_is_nil
+    exception = assert_raises RuntimeError do
+      AdNtLdap.new(nil)
+    end
+    puts exception.message
+    assert_equal(exception.message, "Initialize AdNtLdap was failed. params must not be nil")
   end
 end
