27ddd278133935744f8981ce6094f927c458d532
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-01 18:39:09 +0900

    Updated

--- lib/ad_nt_ldap/ad_nt_ldap.rb
+++ lib/ad_nt_ldap/ad_nt_ldap.rb
@@ -1,4 +1,19 @@
 class AdNtLdap
+  def initialize()
+    puts "no arguments"
+  end
+
+  def initialize(params)
+    raise "Initialize AdNtLdap was failed. params must not be nil" if params == nil
+    #raise 'AdNtLdap requires keys of parameter "ad_host" "ad_binddn" "ad_basedn"' \
+    [:ad_host, :ad_binddn, :ad_basedn].each { |k|
+      raise 'AdNtLdap requires keys in params "ad_host", "ad_binddn", "ad_basedn"' if !params.key?(k)
+    }
+
+    #@nt_host (params['nt_host'])
+    #@nt_port
+  end
+
   def display
     "Hello world"
   end
