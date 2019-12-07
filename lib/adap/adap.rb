3333e20547387d0b49bd31097bc245a4587c54b4
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-07 23:08:33 +0900

    Updated

--- lib/adap/adap.rb
+++ lib/adap/adap.rb
@@ -1,3 +1,5 @@
+require 'net-ldap'
+
 class Adap
   def initialize()
     puts "no arguments"
@@ -24,22 +26,41 @@ class Adap
       raise 'Adap requires keys in params ":ad_host", ":ad_binddn", ":ad_basedn", ":ldap_host", ":ldap_binddn", ":ldap_basedn"' if !params.key?(k)
     }
 
-    @ad_host        = params['ad_host']
-    @ad_port        = (params['ad_port'] ? params['ad_port'] : '389')
-    @ad_binddn      = params['ad_binddn']
-    @ad_basedn      = params['ad_basedn']
-    @ad_password    = (params['ad_password'] ? params['ad_password'] : nil)
-    @ldap_host      = params['ldap_host']
-    @ldap_binddn    = params['ldap_binddn']
-    @ldap_basedn    = params['basedn']
-    @ldap_password  = (params['ldap_password'] ? params['ldap_password'] : nil)
+    @ad_host        = params[:ad_host]
+    @ad_port        = (params[:ad_port] ? params[:ad_port] : '389')
+    @ad_binddn      = params[:ad_binddn]
+    @ad_basedn      = params[:ad_basedn]
+    @ad_auth        = (params.has_key?(:ad_password) ? { :method => :simple, :username => @ad_binddn, :password => params[:ad_password] } : nil)
+    @ldap_host      = params[:ldap_host]
+    @ldap_binddn    = params[:ldap_binddn]
+    @ldap_basedn    = params[:basedn]
+    @ldap_auth      = (params.has_key?(:ldap_password) ? { :method => :simple, :username => @ldap_binddn, :password => params[:ldap_password] } : nil )
 
-    #@nt_host (params['nt_host'])
-    #@nt_port
+    @ad_conn = Net::LDAP.new(
+      :host => @ad_host,
+      :port => @ad_port,
+      :auth => @ad_auth
+    )
+    #raise "Failed to connect AD at ldap://#{@ad_host}:#{@ad_port} with bindDn #{@ad_binddn}" if !ad.bind
+
+    @ldap_conn = Net::LDAP.new(
+      :host => @ldap_host,
+      :port => @ldap_port,
+      :auth => @ldap_auth
+    )
+    #raise "Failed to connect Ldap at ldap://#{@ldap_host}:#{@ldap_port} with bindDn #{@ldap_binddn}" if !ldap.bind
   end
 
-  def sync_user(uid)
-    
+  def sync_user(cn)
+
+    filter = Net::LDAP::Filter.eq("cn",cn)
+    target = nil
+
+    @ad_conn.search( :base => "CN=Users," + @ad_basedn, :filter => filter ) do |entry|
+      puts "DN: #{entry.dn}"
+    end
+
+    p @ad_conn.get_operation_result
   end
 
   def display
