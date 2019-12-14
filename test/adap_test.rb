a40898832d4308ff507458e18aa1bacf10c2f701
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-14 12:20:10 +0900

    Updated

--- test/adap_test.rb
+++ test/adap_test.rb
@@ -70,7 +70,7 @@ class ModAdapTest < Minitest::Test
         {:objectclass => ["top", "person"], :cn => "foo"},
         "secret"
       )
-      assert_equal({:code => 1, :message => "ERROR: Failed to ldap add - Some error"}, ret)
+      assert_equal({:code => 1, :message => "Failed to ldap add a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
       mock.verify
       mock_get_operation_result.verify
     end
@@ -92,19 +92,44 @@ class ModAdapTest < Minitest::Test
     )
     # @ldap_client.get_operation_result.code of @ldap_client.add
     mock_get_operation_result.expect(:code, 0, [])
+    mock.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.modify
     mock.expect(
       :modify, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
-        :attributes => {
-          :objectclass => ["top", "person"],
-          :cn => "foo"
-        }
+        :operations => [
+          [:add, :userPassword, "secret"]
+        ]
       }]
     )
+    # @ldap_client.get_operation_result.code of @ldap_client.modify
+    mock_get_operation_result.expect(:code, 1, [])
+    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    # @ldap_client.get_operation_result.code of @ldap_client.modify
+    mock_get_operation_result.expect(:code, 1, [])
+    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    # @ldap_client.get_operation_result.code of @ldap_client.modify
+    mock_get_operation_result.expect(:error_message, "Some error", [])
+    mock.expect(:get_operation_result, mock_get_operation_result, [])
 
-    # @ldap_client.modify
-
+    Net::LDAP.stub :new, mock do
+      adap = Adap.new({
+        :ad_host => "localhost",
+        :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+        :ad_basedn => "CN=Users,DC=mysite,DC=example,DC=com",
+        :ldap_host   => "ldap_server",
+        :ldap_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
+        :ldap_basedn => "dc=mysite,dc=example,dc=com"
+      })
+      ret = adap.add_user(
+        "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
+        {:objectclass => ["top", "person"], :cn => "foo"},
+        "secret"
+      )
+      assert_equal({:code => 1, :message => "Failed to ldap modify a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
+      mock.verify
+      mock_get_operation_result.verify
+    end
   end
 
 #  def test_it_does_something_useful
