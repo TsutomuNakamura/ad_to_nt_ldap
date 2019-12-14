ddb7038f924dd12974fbc598bc580c4d8eef0469
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-14 14:57:53 +0900

    Updated

--- test/adap_add_user_test.rb
+++ test/adap_add_user_test.rb
@@ -12,11 +12,19 @@ class ModAdapTest < Minitest::Test
     #   else if failed:
     #     #<OpenStruct extended_response=nil, code=65, error_message="no objectClass attribute", matched_dn="", message="Object Class Violation">
 
-    mock = MiniTest::Mock.new
-    mock_get_operation_result = MiniTest::Mock.new
+    mock_ldap                         = MiniTest::Mock.new
+    mock_adap_create_ldap_attributes  = MiniTest::Mock.new
+    mock_get_operation_result         = MiniTest::Mock.new
+
+    # adap.create_ldap_attributes
+    mock_adap_create_ldap_attributes.expect(
+      :call,
+      {:objectclass => ["top", "person"], :cn => "foo"},
+      [{:objectclass => ["top", "person"], :cn => "foo"}]
+    )
 
     # @ldap_client.add
-    mock.expect(
+    mock_ldap.expect(
       :add, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
         :attributes => {
@@ -28,15 +36,15 @@ class ModAdapTest < Minitest::Test
 
     # @ldap_client.get_operation_result.code
     mock_get_operation_result.expect(:code, 1, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # :ret => get_operation_result.code
     mock_get_operation_result.expect(:code, 1, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # :message => get_operation_result.error_message
     mock_get_operation_result.expect(:error_message, "Some error", [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
 
-    Net::LDAP.stub :new, mock do
+    Net::LDAP.stub :new, mock_ldap do
       adap = Adap.new({
         :ad_host   => "localhost",
         :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
@@ -45,23 +53,35 @@ class ModAdapTest < Minitest::Test
         :ldap_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
         :ldap_basedn => "dc=mysite,dc=example,dc=com"
       })
-      ret = adap.add_user(
-        "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
-        {:objectclass => ["top", "person"], :cn => "foo"},
-        "secret"
-      )
-      assert_equal({:code => 1, :message => "Failed to add a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
-      mock.verify
-      mock_get_operation_result.verify
+
+      adap.stub :create_ldap_attributes, mock_adap_create_ldap_attributes do
+        ret = adap.add_user(
+          "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
+          {:objectclass => ["top", "person"], :cn => "foo"},
+          "secret"
+        )
+        assert_equal({:code => 1, :message => "Failed to add a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
+        mock_ldap.verify
+        mock_adap_create_ldap_attributes.verify
+        mock_get_operation_result.verify
+      end
     end
   end
 
   def test_add_user_should_failed_if_ldap_modify_was_failed
-    mock = MiniTest::Mock.new
-    mock_get_operation_result = MiniTest::Mock.new
+    mock_ldap                         = MiniTest::Mock.new
+    mock_adap_create_ldap_attributes  = MiniTest::Mock.new
+    mock_get_operation_result         = MiniTest::Mock.new
+
+    # adap.create_ldap_attributes
+    mock_adap_create_ldap_attributes.expect(
+      :call,
+      {:objectclass => ["top", "person"], :cn => "foo"},
+      [{:objectclass => ["top", "person"], :cn => "foo"}]
+    )
 
     # @ldap_client.add
-    mock.expect(
+    mock_ldap.expect(
       :add, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
         :attributes => {
@@ -72,9 +92,9 @@ class ModAdapTest < Minitest::Test
     )
     # @ldap_client.get_operation_result.code of @ldap_client.add
     mock_get_operation_result.expect(:code, 0, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.modify
-    mock.expect(
+    mock_ldap.expect(
       :modify, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
         :operations => [
@@ -84,15 +104,15 @@ class ModAdapTest < Minitest::Test
     )
     # @ldap_client.get_operation_result.code of @ldap_client.modify
     mock_get_operation_result.expect(:code, 1, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.get_operation_result.code of @ldap_client.modify
     mock_get_operation_result.expect(:code, 1, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.get_operation_result.code of @ldap_client.modify
     mock_get_operation_result.expect(:error_message, "Some error", [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
 
-    Net::LDAP.stub :new, mock do
+    Net::LDAP.stub :new, mock_ldap do
       adap = Adap.new({
         :ad_host => "localhost",
         :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
@@ -101,23 +121,35 @@ class ModAdapTest < Minitest::Test
         :ldap_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
         :ldap_basedn => "dc=mysite,dc=example,dc=com"
       })
-      ret = adap.add_user(
-        "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
-        {:objectclass => ["top", "person"], :cn => "foo"},
-        "secret"
-      )
-      assert_equal({:code => 1, :message => "Failed to modify a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
-      mock.verify
-      mock_get_operation_result.verify
+
+      adap.stub :create_ldap_attributes, mock_adap_create_ldap_attributes do
+        ret = adap.add_user(
+          "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
+          {:objectclass => ["top", "person"], :cn => "foo"},
+          "secret"
+        )
+        assert_equal({:code => 1, :message => "Failed to modify a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com in add_user() - Some error"}, ret)
+        mock_ldap.verify
+        mock_adap_create_ldap_attributes.verify
+        mock_get_operation_result.verify
+      end
     end
   end
 
   def test_add_user_should_success
-    mock = MiniTest::Mock.new
-    mock_get_operation_result = MiniTest::Mock.new
+    mock_ldap                         = MiniTest::Mock.new
+    mock_adap_create_ldap_attributes  = MiniTest::Mock.new
+    mock_get_operation_result         = MiniTest::Mock.new
+
+    # adap.create_ldap_attributes
+    mock_adap_create_ldap_attributes.expect(
+      :call,
+      {:objectclass => ["top", "person"], :cn => "foo"},
+      [{:objectclass => ["top", "person"], :cn => "foo"}]
+    )
 
     # @ldap_client.add
-    mock.expect(
+    mock_ldap.expect(
       :add, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
         :attributes => {
@@ -128,9 +160,9 @@ class ModAdapTest < Minitest::Test
     )
     # @ldap_client.get_operation_result.code of @ldap_client.add
     mock_get_operation_result.expect(:code, 0, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.modify
-    mock.expect(
+    mock_ldap.expect(
       :modify, true, [{
         :dn => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
         :operations => [
@@ -140,12 +172,12 @@ class ModAdapTest < Minitest::Test
     )
     # @ldap_client.get_operation_result.code of @ldap_client.modify
     mock_get_operation_result.expect(:code, 0, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
     # @ldap_client.get_operation_result.code of @ldap_client.modify
     mock_get_operation_result.expect(:code, 0, [])
-    mock.expect(:get_operation_result, mock_get_operation_result, [])
+    mock_ldap.expect(:get_operation_result, mock_get_operation_result, [])
 
-    Net::LDAP.stub :new, mock do
+    Net::LDAP.stub :new, mock_ldap do
       adap = Adap.new({
         :ad_host => "localhost",
         :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
@@ -154,14 +186,18 @@ class ModAdapTest < Minitest::Test
         :ldap_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
         :ldap_basedn => "dc=mysite,dc=example,dc=com"
       })
-      ret = adap.add_user(
-        "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
-        {:objectclass => ["top", "person"], :cn => "foo"},
-        "secret"
-      )
-      assert_equal({:code => 0, :message => nil}, ret)
-      mock.verify
-      mock_get_operation_result.verify
+
+      adap.stub :create_ldap_attributes, mock_adap_create_ldap_attributes do
+        ret = adap.add_user(
+          "uid=foo,ou=Users,dc=mysite,dc=example,dc=com",
+          {:objectclass => ["top", "person"], :cn => "foo"},
+          "secret"
+        )
+        assert_equal({:code => 0, :message => nil}, ret)
+        mock_ldap.verify
+        mock_adap_create_ldap_attributes.verify
+        mock_get_operation_result.verify
+      end
     end
   end
 end
