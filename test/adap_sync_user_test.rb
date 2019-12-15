070b9780f9e0f61a295ce628fdbb868c58748cfd
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-15 22:32:33 +0900

    Updated

--- test/adap_sync_user_test.rb
+++ test/adap_sync_user_test.rb
@@ -97,4 +97,115 @@ class ModAdapTest < Minitest::Test
     ret = adap.sync_user("foo")
     assert_equal({:code => 1, :message => "Failed to get a user uid=foo,ou=Users,dc=mysite,dc=example,dc=com from LDAP - Some error"}, ret)
   end
+
+  def test_sync_user_should_success_if_add_user_returns_success
+    mock_ad_client                      = mock()
+    mock_ldap_client                    = mock()
+    mock_ad_get_operation_result        = mock()
+    mock_ldap_get_operation_result      = mock()
+
+    Adap.expects(:get_ad_client_instance)
+      .with("localhost", 389, { :method => :simple, :username => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com", :password => "ad_secret" })
+      .returns(mock_ad_client)
+
+    Adap.expects(:get_ldap_client_instance)
+      .with("ldap_server", 389, { :method => :simple, :username => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com", :password => "ldap_secret" })
+      .returns(mock_ldap_client)
+
+    # @ad_client.search()
+    mock_ad_client.expects(:search)
+      .with({:base => "CN=foo,CN=Users,DC=mysite,DC=example,DC=com"})
+      .yields({:objectclass => ["top", "person"], :cn => "ad"})
+
+    mock_ldap_client.expects(:search)
+      .with({:base => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com"})
+      .yields(nil)
+
+    ## ad_client != nil and ldap_client == nil -> add_user
+
+    # @ad_client.get_operation_result.code
+    mock_ad_get_operation_result.expects(:code).returns(0)
+    # @ad_client.get_operation_result
+    mock_ad_client.expects(:get_operation_result).returns(mock_ad_get_operation_result)
+
+    # @ldap_client.get_operation_result.code
+    mock_ldap_get_operation_result.expects(:code).returns(0)
+    mock_ldap_client.expects(:get_operation_result).returns(mock_ldap_get_operation_result)
+
+    # Testing from here
+    adap = Adap.new({
+      :ad_host        => "localhost",
+      :ad_binddn      => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_basedn      => "CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_password    => "ad_secret",
+      :ldap_host      => "ldap_server",
+      :ldap_binddn    => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
+      :ldap_basedn    => "dc=mysite,dc=example,dc=com",
+      :ldap_password  => "ldap_secret"
+    })
+    adap.expects(:get_ad_dn).returns("CN=foo,CN=Users,DC=mysite,DC=example,DC=com")
+    adap.expects(:get_ldap_dn).returns("uid=foo,ou=Users,dc=mysite,dc=example,dc=com")
+    adap.expects(:get_password).with("foo").returns("secret")
+    adap.expects(:add_user)
+      .with("uid=foo,ou=Users,dc=mysite,dc=example,dc=com", {:objectclass => ["top", "person"], :cn => "ad"}, "secret")
+      .returns({:code => 0, :message => "Success add_user"})
+
+    ret = adap.sync_user("foo")
+    assert_equal({:code => 0, :message => "Success add_user"}, ret)
+  end
+
+  def test_sync_user_should_success_if_delete_user_returns_success
+    mock_ad_client                      = mock()
+    mock_ldap_client                    = mock()
+    mock_ad_get_operation_result        = mock()
+    mock_ldap_get_operation_result      = mock()
+
+    Adap.expects(:get_ad_client_instance)
+      .with("localhost", 389, { :method => :simple, :username => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com", :password => "ad_secret" })
+      .returns(mock_ad_client)
+
+    Adap.expects(:get_ldap_client_instance)
+      .with("ldap_server", 389, { :method => :simple, :username => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com", :password => "ldap_secret" })
+      .returns(mock_ldap_client)
+
+    # @ad_client.search()
+    mock_ad_client.expects(:search)
+      .with({:base => "CN=foo,CN=Users,DC=mysite,DC=example,DC=com"})
+      .yields(nil)
+
+    mock_ldap_client.expects(:search)
+      .with({:base => "uid=foo,ou=Users,dc=mysite,dc=example,dc=com"})
+      .yields({:objectclass => ["top", "person"], :cn => "ad"})
+
+    ## ad_client == nil and ldap_client != nil -> delete_user
+
+    # @ad_client.get_operation_result.code
+    mock_ad_get_operation_result.expects(:code).returns(0)
+    # @ad_client.get_operation_result
+    mock_ad_client.expects(:get_operation_result).returns(mock_ad_get_operation_result)
+
+    # @ldap_client.get_operation_result.code
+    mock_ldap_get_operation_result.expects(:code).returns(0)
+    mock_ldap_client.expects(:get_operation_result).returns(mock_ldap_get_operation_result)
+
+    # Testing from here
+    adap = Adap.new({
+      :ad_host        => "localhost",
+      :ad_binddn      => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_basedn      => "CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_password    => "ad_secret",
+      :ldap_host      => "ldap_server",
+      :ldap_binddn    => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
+      :ldap_basedn    => "dc=mysite,dc=example,dc=com",
+      :ldap_password  => "ldap_secret"
+    })
+    adap.expects(:get_ad_dn).returns("CN=foo,CN=Users,DC=mysite,DC=example,DC=com")
+    adap.expects(:get_ldap_dn).returns("uid=foo,ou=Users,dc=mysite,dc=example,dc=com")
+    adap.expects(:delete_user)
+      .with("uid=foo,ou=Users,dc=mysite,dc=example,dc=com")
+      .returns({:code => 0, :message => "Success add_user"})
+
+    ret = adap.sync_user("foo")
+    assert_equal({:code => 0, :message => "Success add_user"}, ret)
+  end
 end
