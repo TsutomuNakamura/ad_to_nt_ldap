1a7523c5aed0a7d845fbb5c04e48c42eabd5b129
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2019-12-18 23:32:01 +0900

    Updated

--- test/adap_get_password_test.rb
+++ test/adap_get_password_test.rb
@@ -1,4 +1,33 @@
 require "test_helper"
 
 class ModAdapTest < Minitest::Test
+  def test_get_password_should_return_error_if_getting_password_has_failed
+    mock_ad_client                  = mock()
+    mock_ldap_client                = mock()
+    #mock_ad_get_operation_result    = mock()
+    mock_ldap_get_operation_result  = mock()
+
+    Adap.expects(:get_ad_client_instance)
+      .with("localhost", 389, { :method => :simple, :username => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com", :password => "ad_secret" })
+      .returns(mock_ad_client)
+
+    Adap.expects(:get_ldap_client_instance)
+      .with("ldap_server", 389, { :method => :simple, :username => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com", :password => "ldap_secret" })
+      .returns(mock_ldap_client)
+
+    adap = Adap.new({
+      :ad_host => "localhost",
+      :ad_binddn => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_basedn => "CN=Users,DC=mysite,DC=example,DC=com",
+      :ad_password => "ad_secret",
+      :ldap_host   => "ldap_server",
+      :ldap_binddn => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
+      :ldap_basedn => "dc=mysite,dc=example,dc=com",
+      :ldap_password => "ldap_secret"
+    })
+    adap.expects(:get_raw_password).with("foo").returns("secret")
+
+    result = adap.get_password("foo")
+    assert_equal("secret", result)
+  end
 end
