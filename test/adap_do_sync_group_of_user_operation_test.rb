17bc55ef4e8d72213db127465cfcdde146065530
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-01-25 15:50:52 +0900

    Updated

--- test/adap_do_sync_group_of_user_operation_test.rb
+++ test/adap_do_sync_group_of_user_operation_test.rb
@@ -3,9 +3,17 @@ require "test_helper"
 class ModAdapTest < Minitest::Test
   def test_do_sync_group_of_user_operation_should_return_0_if_the_length_of_operation_was_0
     mock = mock_ad_and_ldap_connections()
-    adap = get_general_adap_instance()
 
+    adap = get_general_adap_instance()
     ret = adap.do_sync_group_of_user_operation({})
-    assert_equal({:code => 0, :operations => nil, :message => "There are not any groups of user to sync"})
+    assert_equal({:code => 0, :operations => nil, :message => "There are not any groups of user to sync"}, ret)
+  end
+
+  def test_do_sync_group_of_user_operation_should_
+    mock = mock_ad_and_ldap_connections()
+
+    adap = get_general_adap_instance()
+    ret = adap.do_sync_group_of_user_operation({})
+    assert_equal({:code => 0, :operations => nil, :message => "There are not any groups of user to sync"}, ret)
   end
 end
