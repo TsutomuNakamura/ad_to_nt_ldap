7041f072f33130de10623b9ca5579173fc41b569
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-02-15 20:51:15 +0900

    Updated

--- test/adap_create_modify_operations_test.rb
+++ test/adap_create_modify_operations_test.rb
@@ -133,12 +133,31 @@ class ModAdapTest < Minitest::Test
       }
     })
     ret = adap.create_modify_operations(
-      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげ株式会社"},
+      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげかぶしきがいしゃ"},
       {:cn => "cn", :sn => "sn"},
       "ad_secret"
     )
     assert_equal([
-      [:add, :'companyname;lang-ja;phonetic', "ほげ株式会社"],
+      [:add, :'companyname;lang-ja;phonetic', "ほげかぶしきがいしゃ"],
+      [:replace, :userpassword, "ad_secret"]
+    ], ret)
+  end
+
+  def test_create_modify_operations_should_create_operation_that_has_add_msds_phonetics
+    adap = get_general_adap_instance({
+      :map_msds_phonetics => {
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic',
+        :'msds-phoneticdepartment' => :'department;lang-ja;phonetic'
+      }
+    })
+    ret = adap.create_modify_operations(
+      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげかぶしきがいしゃ", :'msds-phoneticdepartment' => "かいはつぶ"},
+      {:cn => "cn", :sn => "sn"},
+      "ad_secret"
+    )
+    assert_equal([
+      [:add, :'companyname;lang-ja;phonetic', "ほげかぶしきがいしゃ"],
+      [:add, :'department;lang-ja;phonetic', "かいはつぶ"],
       [:replace, :userpassword, "ad_secret"]
     ], ret)
   end
@@ -150,12 +169,31 @@ class ModAdapTest < Minitest::Test
       }
     })
     ret = adap.create_modify_operations(
-      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげ株式会社"},
-      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ふが株式会社"},
+      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげかぶしきがいしゃ"},
+      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ふがかぶしきがいしゃ"},
       "ad_secret"
     )
     assert_equal([
-      [:replace, :'companyname;lang-ja;phonetic', "ほげ株式会社"],
+      [:replace, :'companyname;lang-ja;phonetic', "ほげかぶしきがいしゃ"],
+      [:replace, :userpassword, "ad_secret"]
+    ], ret)
+  end
+
+  def test_create_modify_operations_should_create_operation_that_has_replace_msds_phonetics
+    adap = get_general_adap_instance({
+      :map_msds_phonetics => {
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic',
+        :'msds-phoneticdepartment' => :'department;lang-ja;phonetic'
+      }
+    })
+    ret = adap.create_modify_operations(
+      {:cn => "cn", :sn => "sn", :'msds-phoneticcompanyname' => "ほげかぶしきがいしゃ", :'msds-phoneticdepartment' => "かいはつぶ"},
+      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ふがかぶしきがいしゃ", :'department;lang-ja;phonetic' => "えいぎょうぶ"},
+      "ad_secret"
+    )
+    assert_equal([
+      [:replace, :'companyname;lang-ja;phonetic', "ほげかぶしきがいしゃ"],
+      [:replace, :'department;lang-ja;phonetic', "かいはつぶ"],
       [:replace, :userpassword, "ad_secret"]
     ], ret)
   end
@@ -168,7 +206,7 @@ class ModAdapTest < Minitest::Test
     })
     ret = adap.create_modify_operations(
       {:cn => "cn", :sn => "sn"},
-      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ほげ株式会社"},
+      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ほげかぶしきがいしゃ"},
       "ad_secret"
     )
     assert_equal([
@@ -177,4 +215,68 @@ class ModAdapTest < Minitest::Test
     ], ret)
   end
 
+  def test_create_modify_operations_should_create_operation_that_has_delete_a_msds_phonetic
+    adap = get_general_adap_instance({
+      :map_msds_phonetics => {
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic',
+        :'msds-phoneticdepartment' => :'department;lang-ja;phonetic'
+      }
+    })
+    ret = adap.create_modify_operations(
+      {:cn => "cn", :sn => "sn"},
+      {:cn => "cn", :sn => "sn", :'companyname;lang-ja;phonetic' => "ほげかぶしきがいしゃ", :'department;lang-ja;phonetic' => "かいはつぶ"},
+      "ad_secret"
+    )
+    assert_equal([
+      [:delete, :'companyname;lang-ja;phonetic', nil],
+      [:delete, :'department;lang-ja;phonetic', nil],
+      [:replace, :userpassword, "ad_secret"]
+    ], ret)
+  end
+
+  def test_create_modify_operations_should_create_operation_that_has_multi_operations
+    adap = get_general_adap_instance({
+      :map_msds_phonetics => {
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic',
+        :'msds-phoneticdepartment' => :'department;lang-ja;phonetic',
+        :'msds-phoneticfirstname' => :'firstname;lang-ja;phonetic',
+        :'msds-phoneticlastname' => :'lastname;lang-ja;phonetic',
+        :'msds-phoneticdisplayname' => :'displayname;lang-ja;phonetic'
+      }
+    })
+    ret = adap.create_modify_operations(
+      {
+        :cn => "ad_cn",                                         # replace
+        :sn => "ad_sn",                                         # replace
+        :uid => "taro-suzuki",                                  # add
+        :'msds-phoneticcompanyname' => "ほげかぶしきがいしゃ",  # add
+        :'msds-phoneticdepartment' => "かいはつぶ",             # add
+        :'msds-phoneticfirstname' => "たろう",                  # replace
+        :'msds-phoneticlastname' => "すずき"                   # replace
+      },
+      {
+        :cn => "ldap_cn",
+        :sn => "ldap_sn",
+        :uidnumber => 1000,                                     # delete
+        :gidnumber => 1000,                                     # delete
+        :'firstname;lang-ja;phonetic' => "じろう",
+        :'lastname;lang-ja;phonetic' => "たなか",
+        :'displayname;lang-ja;phonetic' => "たなか　じろう"    # delete
+      },
+      "ad_secret"
+    )
+    assert_equal([
+      [:replace, :cn, "ad_cn"],
+      [:replace, :sn, "ad_sn"],
+      [:add, :uid, "taro-suzuki"],
+      [:add, :'companyname;lang-ja;phonetic', "ほげかぶしきがいしゃ"],
+      [:add, :'department;lang-ja;phonetic', "かいはつぶ"],
+      [:replace, :'firstname;lang-ja;phonetic', "たろう"],
+      [:replace, :'lastname;lang-ja;phonetic', "すずき"],
+      [:delete, :uidnumber, nil],
+      [:delete, :gidnumber, nil],
+      [:delete, :'displayname;lang-ja;phonetic', nil],
+      [:replace, :userpassword, "ad_secret"]
+    ], ret)
+  end
 end
