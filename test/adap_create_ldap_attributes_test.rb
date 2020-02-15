29967f9bf67d31fd2afdc9c824732b1c8810928d
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2020-02-15 11:21:38 +0900

    Updated

--- test/adap_create_ldap_attributes_test.rb
+++ test/adap_create_ldap_attributes_test.rb
@@ -44,17 +44,9 @@ class ModAdapTest < Minitest::Test
 
   def test_create_ldap_attribute_should_sync_one_phonetic
     mock = mock_ad_and_ldap_connections()
-    adap = Adap.new({
-      :ad_host        => "localhost",
-      :ad_binddn      => "CN=Administrator,CN=Users,DC=mysite,DC=example,DC=com",
-      :ad_basedn      => "DC=mysite,DC=example,DC=com",
-      :ad_password    => "ad_secret",
-      :ldap_host      => "ldap_server",
-      :ldap_binddn    => "uid=Administrator,ou=Users,dc=mysite,dc=example,dc=com",
-      :ldap_basedn    => "dc=mysite,dc=example,dc=com",
-      :ldap_password  => "ldap_secret",
+    adap = get_general_adap_instance({
       :map_msds_phonetics => {
-        :'msds-phoneticcompanyname' => :'companyName;lang-ja;phonetic'
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic'
       }
     })
 
@@ -69,30 +61,70 @@ class ModAdapTest < Minitest::Test
         :objectclass => ["top", "person", "organizationalPerson", "inetOrgPerson", "posixAccount", "shadowAccount"],
         :uid => ["taro-suzuki"],
         :sn => ["鈴木太郎"],
-        :'companyName;lang-ja;phonetic' => ["ほげ株式会社"]
+        :'companyname;lang-ja;phonetic' => ["ほげ株式会社"]
       },
       result
     )
   end
 
   def test_create_ldap_attribute_should_not_sync_one_phonetic
-    # TODO
+    mock = mock_ad_and_ldap_connections()
+    adap = get_general_adap_instance()
+
+    result = adap.create_ldap_attributes({
+      "uid"               => ["taro-suzuki"],
+      "sn"                => ["鈴木太郎"],
+      "msDS-PhoneticCompanyName" => ["ほげ株式会社"]    # <- Expect not be synched
+    })
+
+    assert_equal(
+      {
+        :objectclass => ["top", "person", "organizationalPerson", "inetOrgPerson", "posixAccount", "shadowAccount"],
+        :uid => ["taro-suzuki"],
+        :sn => ["鈴木太郎"]
+      },
+      result
+    )
   end
 
   def test_create_ldap_attribute_should_sync_phonetics
-    # TODO
+    mock = mock_ad_and_ldap_connections()
+    adap = get_general_adap_instance({
+      :map_msds_phonetics => {
+        :'msds-phoneticcompanyname' => :'companyname;lang-ja;phonetic',
+        :'msds-phoneticdisplayname' => :'displayname;lang-ja;phonetic'
+      }
+    })
+
+    result = adap.create_ldap_attributes({
+      "uid"               => ["taro-suzuki"],
+      "sn"                => ["鈴木太郎"],
+      "msDS-PhoneticCompanyName" => ["ほげ株式会社"],
+      "msDS-PhoneticDisplayName" => ["てすと"]
+    })
+
+    assert_equal(
+      {
+        :objectclass => ["top", "person", "organizationalPerson", "inetOrgPerson", "posixAccount", "shadowAccount"],
+        :uid => ["taro-suzuki"],
+        :sn => ["鈴木太郎"],
+        :'companyname;lang-ja;phonetic' => ["ほげ株式会社"],
+        :'displayname;lang-ja;phonetic' => ["てすと"],
+      },
+      result
+    )
   end
 
-  def test_create_ldap_attribute_should_not_sync_phonetics
-    # TODO
-  end
-
-  def test_create_ldap_attribute_should_sync_one_phonetic_and_not_sync_another_phonetic
-    # TODO
-  end
-
-  def test_create_ldap_attribute_should_sync_phonetics_and_not_sync_others
-    # TODO
-  end
+#  def test_create_ldap_attribute_should_not_sync_phonetics
+#    # TODO
+#  end
+#
+#  def test_create_ldap_attribute_should_sync_one_phonetic_and_not_sync_another_phonetic
+#    # TODO
+#  end
+#
+#  def test_create_ldap_attribute_should_sync_phonetics_and_not_sync_others
+#    # TODO
+#  end
 end
 
